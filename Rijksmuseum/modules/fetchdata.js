a7dd196355edb86356367e4d1a39026772d3a78f
Menno Vlaming <mennovlaming@mbp-van-menno.kpn>
2023-03-08 21:03:24 +0100

    update

--- Rijksmuseum/modules/fetchdata.js
+++ Rijksmuseum/modules/fetchdata.js
@@ -2,7 +2,7 @@ import $ from "./querySelector.js"
 import renderData from "./render.js"
 
 const apiURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=wJjmayJK'
-const loader = document.querySelector("section:first-of-type")
+const loader = document.querySelector(".loadingstate")
 const section = $('section:nth-of-type(2')
 
 function fetchData (){
@@ -17,15 +17,15 @@ function fetchData (){
                         // Set global state of articles;
                         window.articles = data.artObjects;
                         renderData(window.articles);
-                
+                   
                     })
 
                     .catch(error => {
                         // Handle errors
                         console.error(error);
-                    
+                        // window.alert("De data kan niet geladen worden, probeer het later opnieuw");
                         // Hide the loading state
-                         loader.style.display = 'none';
+                          loader.style.display = 'none';
                       });
 }
 
