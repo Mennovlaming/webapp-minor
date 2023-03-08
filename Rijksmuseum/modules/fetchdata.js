0767f3602be0948db1cede67c748f0a6c194cc65
Menno Vlaming <mennovlaming@mbp-van-menno.kpn>
2023-03-08 20:21:37 +0100

    test

--- Rijksmuseum/modules/fetchdata.js
+++ Rijksmuseum/modules/fetchdata.js
@@ -2,18 +2,18 @@ import $ from "./querySelector.js"
 import renderData from "./render.js"
 
 const apiURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=wJjmayJK'
-const loader = document.querySelector(".loadingstate")
+const loader = document.querySelector("section:first-of-type")
 const section = $('section:nth-of-type(2')
 
 function fetchData (){
     fetch(apiURL)
                     .then(response => response.json())
                     .then(data => {
-                    
-                    loader.style.display = 'none';
+                     
+                     loader.style.display = 'none';
 
                     // changeHTML(data) 
-                    
+                    console.log(data)
                         // Set global state of articles;
                         window.articles = data.artObjects;
                         renderData(window.articles);
@@ -25,7 +25,7 @@ function fetchData (){
                         console.error(error);
                     
                         // Hide the loading state
-                        loader.style.display = 'none';
+                         loader.style.display = 'none';
                       });
 }
 
